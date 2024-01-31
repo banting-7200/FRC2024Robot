c3e1b627605d6fe41a0db7643a50cd46531ce830
jsrii <jas.kanthan@gmail.com>
2024-01-30 22:31:02 -0500

    Creep drive, have not tested yet

---
+++ src/main/java/frc/robot/subsystems/swervedrive/buttonSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.swervedrive;
+import frc.robot.RobotContainer;
+
+public class buttonSubsystem {
+    public static final double maxNormalSpeed = 14.5;
+    public static final double creepSpeed = 5;
+
+    public Double getMaximumSpeed() {
+        if (RobotContainer.creepBoolean.get()) {
+            return maxNormalSpeed;
+        } else {
+            return creepSpeed+1;
+        }
+    }  
+}

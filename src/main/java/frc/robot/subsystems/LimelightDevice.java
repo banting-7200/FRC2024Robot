928edd578b8b0a4b94ac2880105da295366ecbdb
Isaac MacKenzie <isaacmackenzie75@gmail.com>
2024-02-09 13:57:53 -0500

    Cleaned up & organized functions. Renamed some to make consistant across class. May interfere with older code.

--- src/main/java/frc/robot/subsystems/LimelightDevice.java
+++ src/main/java/frc/robot/subsystems/LimelightDevice.java
@@ -11,12 +11,6 @@ public class LimelightDevice extends SubsystemBase {
   private NetworkTable mainTable;
   public int mode;
 
-  private double k_XP = 0.01;
-  private double k_fowardP = 5;
-  private double targetArea = 0.4;
-
-  // NetworkTableEntry m_XP, m_YP, m_targetArea;
-
   public LimelightDevice() { // initializes device
     mainTable =
         NetworkTableInstance.getDefault().getTable("limelight"); // gets the network table with key
@@ -35,10 +29,38 @@ public class LimelightDevice extends SubsystemBase {
     mainTable.getEntry("pipeline").setNumber(selection);
   }
 
-  public int
-      getTagData() { // posts limelight apriltag pipeline data to SmartDashboard & returns id of
-    // apriltag. Returns -1 if no tag is detected.
+  public int getTagID() { // returns id of apriltag or -1 if no tag is detected.
+    double tid = mainTable.getEntry("tid").getDouble(-1);
+    SmartDashboard.putNumber("AprilTag ID", tid);
+    return (int) tid;
+  }
 
+  public boolean tagDetected() { // returns true if tag is detected
+    double ttarget = mainTable.getEntry("tv").getDouble(0);
+    boolean tdetected = ttarget == 0 ? false : true;
+    SmartDashboard.putBoolean("AprilTag Detected", tdetected);
+    return tdetected;
+  }
+
+  public double getTagArea() { // return tag area
+    double ta = mainTable.getEntry("ta").getDouble(0);
+    SmartDashboard.putNumber("Tag Area", ta);
+    return ta;
+  }
+
+  public double getTagX() { // return tag x value (horizontal across camera screen)
+    double tx = mainTable.getEntry("tx").getDouble(0);
+    SmartDashboard.putNumber("Tag X", tx);
+    return tx;
+  }
+
+  public double getTagY() { // return tag y value (vertical across camera screen)
+    double ty = mainTable.getEntry("ty").getDouble(0);
+    SmartDashboard.putNumber("Tag Y", ty);
+    return ty;
+  }
+
+  public void putTagData() { // publishes tag data to SmartDashboard
     double ttarget = mainTable.getEntry("tv").getDouble(0); // may have to be double then converted
     double tx = mainTable.getEntry("tx").getDouble(0);
     double ty = mainTable.getEntry("ty").getDouble(0);
@@ -51,34 +73,12 @@ public class LimelightDevice extends SubsystemBase {
     SmartDashboard.putNumber("Tag Y", ty);
     SmartDashboard.putNumber("Tag Area", ta);
     SmartDashboard.putNumber("AprilTag ID", tid);
-
-    return (int) tid;
-  }
-
-  public boolean tagDetected() {
-    double ttarget = mainTable.getEntry("tv").getDouble(0);
-    return ttarget == 0 ? false : true;
-  }
-
-  public Pose2d
-      getFakeTagPose() { // Can't take negative values because it is trying to pathfind out of the
-    // feild
-    return new Pose2d(2.98, 4, new Rotation2d(33));
-  }
-
-  public double getTagArea() {
-    return mainTable.getEntry("ta").getDouble(0);
-  }
-
-  public double getTX() {
-    return mainTable.getEntry("tx").getDouble(0);
   }
 
   public boolean
-      getNoteData() { // posts limelight note pipelinedata to SmartDashboard & returns true if note
+      noteDetected() { // posts limelight note pipelinedata to SmartDashboard & returns true if note
     // is
     // detected
-
     double ntarget = mainTable.getEntry("tv").getDouble(0); // may have to be double then converted
     double nx = mainTable.getEntry("tx").getDouble(0);
     double ny = mainTable.getEntry("ty").getDouble(0);
@@ -89,7 +89,11 @@ public class LimelightDevice extends SubsystemBase {
     SmartDashboard.putNumber("Note X", nx);
     SmartDashboard.putNumber("Note Y", ny);
     SmartDashboard.putNumber("Note Size", na);
-
     return ndetected;
+  }
+
+  public Pose2d getFakeTagPose() { // input fake tag values for simulation
+    // Can't take negative values because it is trying to pathfind out of the feild
+    return new Pose2d(2.98, 4, new Rotation2d(33));
   }
 }

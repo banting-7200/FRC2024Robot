da80d02a71def5d222a0a44f92894a99cb44ae8d
Logan Knott <loganeknott@gmail.com>
2024-02-12 15:31:01 -0500

    Added degree to rotation conversion and absolute encoder
    Added a function to convert degrees to motor rotations and support for absolute encoders. #1

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.REVPhysicsSim;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -16,7 +18,7 @@ import frc.robot.Constants.Arm;
 public class ArmSubsystem extends SubsystemBase {
 
     private CANSparkMax leftArmMotor, rightArmMotor;
-    private RelativeEncoder rightEncoder;
+    private AbsoluteEncoder rightEncoder;// Change to absolute encoder???
     private SparkPIDController pidController;
 
     SolenoidActions shooterSolenoidActions = new SolenoidActions(Constants.m_pH.makeSolenoid(Arm.s_Channel));
@@ -33,16 +35,16 @@ public class ArmSubsystem extends SubsystemBase {
         leftArmMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
         rightArmMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
 
-        rightEncoder = rightArmMotor.getEncoder();
+        rightEncoder = rightArmMotor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
         rightEncoder.setPositionConversionFactor(42);// Amount of ticks in a Neo encoder rotation unit. Converted for gravity feedfoward later on.
         pidController = rightArmMotor.getPIDController();
         pidController.setFeedbackDevice(rightEncoder);
 
         setPID(Arm.p, Arm.i, Arm.d, Arm.f, Arm.iz);
 
-          if (RobotBase.isSimulation()) {
-      REVPhysicsSim.getInstance().addSparkMax(rightArmMotor, DCMotor.getNEO(1));
-    }
+        if (RobotBase.isSimulation()) {
+            REVPhysicsSim.getInstance().addSparkMax(rightArmMotor, DCMotor.getNEO(1));
+        }
     }
 
     private void setPID(double p, double i, double d, double f, double iz) {
@@ -57,7 +59,6 @@ public class ArmSubsystem extends SubsystemBase {
         pidController.setOutputRange(Arm.pidOutputMin, Arm.pidOutputMax, smartMotionSlot);
 
         // Configure smart motion
-      
         pidController.setSmartMotionMaxVelocity(Arm.maxMotorVelocity, smartMotionSlot);
         pidController.setSmartMotionMinOutputVelocity(Arm.minMotorVelocity, smartMotionSlot);
         pidController.setSmartMotionMaxAccel(Arm.maxMotorAccel, smartMotionSlot);
@@ -65,39 +66,44 @@ public class ArmSubsystem extends SubsystemBase {
     }
 
     public void moveToAngle(double angle) {
-        pidController.setReference(angle, CANSparkMax.ControlType.kSmartMotion, Arm.smartMotionSlot, getArbFF());// Todo: find what pidslot the pid is or find a way to apply arb feedfoward without it
+        pidController.setReference(angle, CANSparkMax.ControlType.kSmartMotion, Arm.smartMotionSlot, getArbFF());
         System.out.println("motor angle: " + rightEncoder.getPosition());
     }
 
     public double getArbFF(){
- // Arbirtary feedfoward to account for gravity acting on the arm
+        // Arbirtary feedfoward to account for gravity acting on the arm
         int kMeasuredPosHorizontal = 840; // Default position measured when arm is horizontal from example. Todo: find
                                           // the value for our arm.
-        double kTicksPerDegree = 42 / 360; // Todo: find the ammount of ticks per degree relative to all the gearboxes
-                                           // and factor that into the calculation.
+        double kTicksPerDegree = 42 / (360 * 200); // Todo: find the ammount of ticks per degree relative to all the gearboxes and factor that into the calculation.
         double currentPos = rightEncoder.getPosition();
         double degrees = (currentPos - kMeasuredPosHorizontal) / kTicksPerDegree;
         double radians = java.lang.Math.toRadians(degrees);
         double cosineScalar = java.lang.Math.cos(radians);
 
-        double maxGravityFF = 0.07;// Todo: Find the best gravity feed foward for our arm
+        double maxGravityFF = 0.07;// Todo: Find the best gravity feed forward for our arm
 
         return maxGravityFF * cosineScalar;
     }
 
     //Todo: add a degrees to motor rotations conversion to account for the gearboxes
+    double degreesToRotations(double degrees){
+          return degrees * 200;
+    }
     
     // Solenoid movement functions
     public void tuckShooter(){
         shooterSolenoidActions.setOff();
+        System.out.println("Shooter tucked");
     }
 
     public void deployShooter(){
         shooterSolenoidActions.setOn();
+        System.out.println("Shooter deployed");
     }
 
     public void toggleShooterState(){
         shooterSolenoidActions.toggle();
+        System.out.println("Shooter toggled");
     }
 
     public boolean isTucked() {

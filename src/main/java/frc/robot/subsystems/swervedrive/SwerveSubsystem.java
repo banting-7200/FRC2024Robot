cd34d13226ea0839d8fa996ccbeff35558cb1c93
Isaac MacKenzie <isaacmackenzie75@gmail.com>
2024-03-21 19:32:35 -0400

    auto pids tuned

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -41,7 +41,7 @@ public class SwerveSubsystem extends SubsystemBase {
   private final SwerveDrive swerveDrive;
 
   /** Maximum speed of the robot in meters per second, used to limit acceleration. */
-  public double maximumSpeed = Units.feetToMeters(14.5);
+  public double maximumSpeed = Units.feetToMeters(16);
 
   /* speed multiplier for creep */
   private double speedMultiplier = 1;
@@ -114,18 +114,12 @@ public class SwerveSubsystem extends SubsystemBase {
         new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in
             // your Constants class
             new PIDConstants(5.0, 0.0, 0.0),
-            /*  swerveDrive.swerveController.config..p,
-            swerveDrive.swerveController.config.TranslationPID.i,
-            swerveDrive.swerveController.config.TranslationPID.d), */
 
             // todo: check to see if this is necessary (removed in example code)
             // Translation PID constants
-            new PIDConstants(
-                swerveDrive.swerveController.config.headingPIDF.p,
-                swerveDrive.swerveController.config.headingPIDF.i,
-                swerveDrive.swerveController.config.headingPIDF.d),
+            new PIDConstants(5.0, 0.0, 0.0),
             // Rotation PID constants
-            4.41,
+            4.88,
             // Max module speed, in m/s
             swerveDrive.swerveDriveConfiguration.getDriveBaseRadiusMeters(),
             // Drive base radius in meters. Distance from robot center to furthest module.

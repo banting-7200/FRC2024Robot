07b4f574641cb2ca1b8a4654d0b9a1de1bd668a1
jassri <84788771+jsrii@users.noreply.github.com>
2024-03-21 15:05:24 -0400

    Actually Schedule the command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -132,6 +132,10 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
+
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    
+
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }

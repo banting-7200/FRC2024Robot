335d557540859d9ade77d03572b33ac07f0b49ee
Isaac MacKenzie <isaacmackenzie75@gmail.com>
2024-03-21 17:07:33 -0400

    PID values fixed for changed conversion factor (gears changed on robot)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -134,6 +134,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
@@ -143,7 +144,7 @@ public class Robot extends TimedRobot {
     }
     m_robotContainer.setDriveMode();
     m_robotContainer.setMotorBrake(true);
-    limelight.setLight(true);
+    // limelight.setLight(true);
   }
 
   /** This function is called periodically during operator control. */

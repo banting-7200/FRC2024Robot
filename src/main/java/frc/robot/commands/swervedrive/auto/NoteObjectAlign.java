6b2394f6625a443ea046506753ce88d806a70b96
jassri <84788771+jsrii@users.noreply.github.com>
2024-03-25 14:10:16 -0400

    Note Align Changes

--- src/main/java/frc/robot/commands/swervedrive/auto/NoteObjectAlign.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/NoteObjectAlign.java
@@ -1,16 +1,18 @@
 package frc.robot.commands.swervedrive.auto;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.shooter.intakeCommand;
-import frc.robot.subsystems.ArmAndHead.ShooterSubsystem;
-import frc.robot.subsystems.Vision.PhotonCamera;
+import frc.robot.subsystems.PhotonCamera;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
 public class NoteObjectAlign extends Command {
 
-  private final SwerveSubsystem swerveSubsystem;
+  private SwerveSubsystem swerveSubsystem = null;
   private final PhotonCamera photonCam;
 
   private final PIDController positionController;
@@ -20,56 +22,61 @@ public class NoteObjectAlign extends Command {
   private double c_noteArea;
   private double d_noteArea;
 
+  private XboxController driverXbox;
+  private BooleanSupplier isRedAliance;
+
   private long startedMillis;
   private long currentMillis;
 
-  private Command s_command;
-
   public NoteObjectAlign(
-      SwerveSubsystem swerveSubsystem,
-      PhotonCamera photonCam,
-      double d_noteArea,
-      ShooterSubsystem shooter) {
+      SwerveSubsystem swerveSubsystem, PhotonCamera photonCam, double d_noteArea) {
     this.swerveSubsystem = swerveSubsystem;
     this.photonCam = photonCam;
 
     this.d_noteArea = d_noteArea;
-    this.shooter = shooter;
 
     positionController = new PIDController(1, 0, 0);
     positionController.setSetpoint(d_noteArea);
 
     rotationController = new PIDController(0.035, 0.0001, 0);
-    rotationController.setSetpoint(0);
 
-    addRequirements(swerveSubsystem, photonCam);
+    rotationController.setSetpoint(0);
   }
 
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   @Override
   public void execute() {
+    System.out.println("Currently Executing Tag Align command");
+
     double fowardAdjust = 0;
     double rotationAdjust = 0;
 
-    if (photonCam.hasTarget()) {
+    if (photonCam.has_targets()) {
       c_noteArea = photonCam.getNoteArea();
-      fowardAdjust = positionController.calculate(c_noteArea, d_noteArea);
       rotationAdjust = rotationController.calculate(photonCam.getNoteYaw(), 0);
       swerveSubsystem.drive(new Translation2d(fowardAdjust, 0), rotationAdjust, false);
-      if (c_noteArea == (d_noteArea / 2)) {
-        System.out.println("Running intake command within Note Align!");
-        s_command = new intakeCommand(6000, shooter);
-      }
     } else {
-      swerveSubsystem.drive(new Translation2d(0, 0), 0, false);
+
+      swerveSubsystem.drive(
+          new Translation2d(
+              MathUtil.applyDeadband(-joystickSquaredY.get(),
+                  OperatorConstants.LEFT_Y_DEADBAND),
+              MathUtil.applyDeadband(-joystickSquaredX.get(),
+                  OperatorConstants.LEFT_X_DEADBAND)),
+          0,
+          true);
+    }
+    if (s_command == null) {
+      s_command.schedule();
     }
   }
 
   @Override
   public boolean isFinished() {
-    return positionController.atSetpoint() && rotationController.atSetpoint();
+    return rotationController.atSetpoint();
   }
 
   @Override

a8dea7bcb35627c0a3de33d4bfbd31ac950f2f89
Isaac MacKenzie <isaacmackenzie75@gmail.com>
2024-03-26 13:35:43 -0400

    Fixed amp shot delay

--- src/main/java/frc/robot/commands/shooter/shootCommand.java
+++ src/main/java/frc/robot/commands/shooter/shootCommand.java
@@ -3,129 +3,143 @@
  * the shooter.
  */
 
-package frc.robot.commands.shooter;
+ package frc.robot.commands.shooter;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.copilotController;
-import frc.robot.Constants.maxCommandWaitTime;
-import frc.robot.subsystems.ArmAndHead.ShooterSubsystem;
-import frc.robot.subsystems.Feedback.LightSubsystem;
-import frc.robot.subsystems.Feedback.LightSubsystem.LightStates;
-import frc.robot.subsystems.Feedback.ShuffleboardSubsystem;
-import java.time.Clock;
-import java.util.function.BooleanSupplier;
-import java.util.function.IntSupplier;
-
-public class shootCommand extends Command {
-
-  public ShooterSubsystem shooter;
-
-  LightSubsystem lights = LightSubsystem.getInstance();
-  Clock currentTime = Clock.systemDefaultZone();
-
-  long startedMillis; // time when started
-  long currentMillis; // current time
-  long sinceNoteLeft; // time since note left shooter
-
-  IntSupplier rpm; // Int supplier for different shoot rpm's(speaker/amp)
-  Boolean hasSeenNote = false; // if note has been detected yet
-  IntSupplier waitTime; // Int supplier for different wait times(speaker/amp)
-  BooleanSupplier isSpeakerShot;
-
-  Joystick controller;
-
-  ShuffleboardSubsystem shuffle = ShuffleboardSubsystem.getInstance();
-
-  boolean pause;
-
-  public shootCommand(
-      IntSupplier rpm,
-      ShooterSubsystem shooter,
-      IntSupplier waitTime,
-      BooleanSupplier isSpeakerShot) {
-    this.rpm = rpm;
-    this.shooter = shooter;
-    this.waitTime = waitTime;
-    this.isSpeakerShot = isSpeakerShot;
-    this.controller = null;
-
-    addRequirements(shooter);
-  }
-
-  public shootCommand(
-      int rpm, ShooterSubsystem shooter, int waitTime, BooleanSupplier isSpeakerShot) {
-    this(() -> rpm, shooter, () -> waitTime, isSpeakerShot);
-  }
-
-  public shootCommand(
-      IntSupplier rpm,
-      ShooterSubsystem shooter,
-      IntSupplier waitTime,
-      BooleanSupplier isSpeakerShot,
-      Joystick controller) {
-    this(rpm, shooter, waitTime, isSpeakerShot);
-    this.controller = controller;
-  }
-
-  @Override
-  public void initialize() {
-    sinceNoteLeft = 0;
-    startedMillis = currentTime.millis();
-    hasSeenNote = false;
-
-    shooter.stopIntakeMotor(); // Stop the intake motor
-    System.out.println("Started shoot command");
-    System.out.println("is Shoot state: " + isSpeakerShot.getAsBoolean());
-    pause = controller != null ? controller.getRawButton(copilotController.shootButton) : false;
-  }
-
-  @Override
-  public void execute() {
-    currentMillis = currentTime.millis(); // record current time
-
-    if (currentMillis - startedMillis < 100
-        && isSpeakerShot.getAsBoolean()) { // until 100 millis pass
-      shooter.spinIntakeToPositiveRPM(2000); // reverse intake
-      shooter.spinShootNegativeToRPM(500);
-
-    } else if ((currentMillis - startedMillis) > waitTime.getAsInt()) {
-      // waits for 250 ms for it to turn on the shoot
-      // motor
-      shooter.spinIntakeToNegativeRPM(rpm.getAsInt()); // feeds to intake motor
-      // System.out.println("Run Shooter motor");
-    } else {
-      shooter.stopIntakeMotor();
-    }
-    if (!shooter.shooterHasNote()
-        && currentMillis - startedMillis > 100 + waitTime.getAsInt()
-        && !hasSeenNote) {
-      // if it see's the note it will set the since note left time for current
-      // time
-      sinceNoteLeft = currentMillis;
-      hasSeenNote = true;
-      // System.out.println("SAW THE NOTE");
-    }
-    if (currentMillis - startedMillis > 450 /*&& !pause*/) {
-      shooter.spinShootToRPM(rpm.getAsInt());
-    }
-  }
-
-  // System.out.println("Current  HAS NOTE STATE: " + shooter.shooterHasNote());
-
-  public boolean isFinished() {
-    return ((currentMillis - sinceNoteLeft) > /* shuffle.getNumber("shoot Ramp Down") */ 500
-            && hasSeenNote == true)
-        || (currentMillis - startedMillis > maxCommandWaitTime.shootCommandWaitTime);
-  }
-
-  @Override
-  public void end(boolean interrupted) {
-    shooter.stopShootMotor();
-    shooter.stopIntakeMotor();
-    System.out.println("Shooting Done");
-    if (!shooter.shooterHasNote()) lights.SetLightState(LightStates.ReadyForPickup);
-    // System.out.println("Has Note state is currently: " +
-    // shooter.shooterHasNote());
-  }
-}
+ import edu.wpi.first.wpilibj.Joystick;
+ import edu.wpi.first.wpilibj2.command.Command;
+ import frc.robot.Constants.copilotController;
+ import frc.robot.Constants.maxCommandWaitTime;
+ import frc.robot.subsystems.ArmAndHead.ShooterSubsystem;
+ import frc.robot.subsystems.Feedback.LightSubsystem;
+ import frc.robot.subsystems.Feedback.LightSubsystem.LightStates;
+ import frc.robot.subsystems.Feedback.ShuffleboardSubsystem;
+ import java.time.Clock;
+ import java.util.function.BooleanSupplier;
+ import java.util.function.IntSupplier;
+ 
+ public class shootCommand extends Command {
+ 
+   public ShooterSubsystem shooter;
+ 
+   LightSubsystem lights = LightSubsystem.getInstance();
+   Clock currentTime = Clock.systemDefaultZone();
+ 
+   long startedMillis; // time when started
+   long currentMillis; // current time
+   long sinceNoteLeft; // time since note left shooter
+ 
+   IntSupplier rpm; // Int supplier for different shoot rpm's(speaker/amp)
+   Boolean hasSeenNote = false; // if note has been detected yet
+   IntSupplier waitTime; // Int supplier for different wait times(speaker/amp)
+   BooleanSupplier isSpeakerShot;
+ 
+   Joystick controller;
+ 
+   ShuffleboardSubsystem shuffle = ShuffleboardSubsystem.getInstance();
+ 
+   boolean pause;
+ 
+   public shootCommand(
+       IntSupplier rpm,
+       ShooterSubsystem shooter,
+       IntSupplier waitTime,
+       BooleanSupplier isSpeakerShot) {
+     this.rpm = rpm;
+     this.shooter = shooter;
+     this.waitTime = waitTime;
+     this.isSpeakerShot = isSpeakerShot;
+     this.controller = null;
+ 
+     addRequirements(shooter);
+   }
+ 
+   public shootCommand(
+       int rpm, ShooterSubsystem shooter, int waitTime, BooleanSupplier isSpeakerShot) {
+     this(() -> rpm, shooter, () -> waitTime, isSpeakerShot);
+   }
+ 
+   public shootCommand(
+       IntSupplier rpm,
+       ShooterSubsystem shooter,
+       IntSupplier waitTime,
+       BooleanSupplier isSpeakerShot,
+       Joystick controller) {
+     this(rpm, shooter, waitTime, isSpeakerShot);
+     this.controller = controller;
+   }
+ 
+   @Override
+   public void initialize() {
+     sinceNoteLeft = 0;
+     startedMillis = currentTime.millis();
+     hasSeenNote = false;
+ 
+     shooter.stopIntakeMotor(); // Stop the intake motor
+     System.out.println("Started shoot command");
+     System.out.println("is Shoot state: " + isSpeakerShot.getAsBoolean());
+     pause = controller != null ? controller.getRawButton(copilotController.shootButton) : false;
+   }
+ 
+   @Override
+   public void execute() {
+     currentMillis = currentTime.millis(); // record current time
+     if (isSpeakerShot.getAsBoolean()) {
+       if (currentMillis - startedMillis < 100 // pullback
+       ) { // until 100 millis pass
+         shooter.spinIntakeToPositiveRPM(2000); // reverse intake
+         shooter.spinShootNegativeToRPM(500);
+ 
+       } else if ((currentMillis - startedMillis) > waitTime.getAsInt()) { // feed in
+         // waits for 250 ms for it to turn on the shoot
+         // motor
+         shooter.spinIntakeToNegativeRPM(rpm.getAsInt()); // feeds to intake motor
+         // System.out.println("Run Shooter motor");
+       } else {
+         shooter.stopIntakeMotor();
+       }
+       if (!shooter.shooterHasNote()
+           && currentMillis - startedMillis > 100 + waitTime.getAsInt()
+           && !hasSeenNote) {
+         // if it see's the note it will set the since note left time for current
+         // time
+         sinceNoteLeft = currentMillis;
+         hasSeenNote = true;
+         // System.out.println("SAW THE NOTE");
+       }
+       if (currentMillis - startedMillis > 450 /*&& !pause*/) { // rev up
+         shooter.spinShootToRPM(rpm.getAsInt());
+       }
+     } else {
+       if (!shooter.shooterHasNote()
+           && currentMillis - startedMillis > 100 + waitTime.getAsInt()
+           && !hasSeenNote) {
+         // if it see's the note it will set the since note left time for current
+         // time
+         sinceNoteLeft = currentMillis;
+         hasSeenNote = true;
+         // System.out.println("SAW THE NOTE");
+       }
+       shooter.spinShootToRPM(rpm.getAsInt());
+       shooter.spinIntakeToNegativeRPM(rpm.getAsInt());
+     }
+   }
+ 
+   // System.out.println("Current  HAS NOTE STATE: " + shooter.shooterHasNote());
+ 
+   public boolean isFinished() {
+     return ((currentMillis - sinceNoteLeft) > /* shuffle.getNumber("shoot Ramp Down") */ 500
+             && hasSeenNote == true)
+         || (currentMillis - startedMillis > maxCommandWaitTime.shootCommandWaitTime);
+   }
+ 
+   @Override
+   public void end(boolean interrupted) {
+     shooter.stopShootMotor();
+     shooter.stopIntakeMotor();
+     System.out.println("Shooting Done");
+     if (!shooter.shooterHasNote()) lights.SetLightState(LightStates.ReadyForPickup);
+     // System.out.println("Has Note state is currently: " +
+     // shooter.shooterHasNote());
+   }
+ }
+ 

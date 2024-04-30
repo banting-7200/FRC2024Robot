dc8e3c38414b43ff7fb27374230e28405e6703b5
jassri <84788771+jsrii@users.noreply.github.com>
2024-04-30 15:36:28 -0400

    Search Note for full auto

--- src/main/java/frc/robot/commands/swervedrive/auto/NoteAutoStateMachine.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/NoteAutoStateMachine.java
@@ -9,7 +9,8 @@ import frc.robot.subsystems.ArmAndHead.ShooterSubsystem;
 import frc.robot.subsystems.Vision.LimelightDevice;
 import frc.robot.subsystems.Vision.PhotonCamera;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
-
+import frc.robot.commands.swervedrive.auto.SearchNote;
+//src\main\java\frc\robot\commands\swervedrive\auto\SearchNote.java
 public class NoteAutoStateMachine {
   //Enum to classify the different states of the auto
   public enum States {
@@ -25,19 +26,21 @@ public class NoteAutoStateMachine {
   private AprilTagAlign aprilTagAlign;
   private shootCommand shootCommand;
   private MoveArmToPosition armToPosition;
+  private SearchNote searchNote;
 
   public NoteAutoStateMachine(SwerveSubsystem swerveSubsystem, PhotonCamera photonCamera, LimelightDevice limelightDevice, ShooterSubsystem shooter, ArmSubsystem arm) {
     driveToNote = new DriveToNote(swerveSubsystem, photonCamera, 0);
     aprilTagAlign = new AprilTagAlign(swerveSubsystem, limelightDevice, 0, 0, false);//Make the tag id which ever tag we decide goes on the new target
     shootCommand = new shootCommand(Shooter.speakerShootRPM, shooter, Shooter.speakerWaitTime, true);//The shoot rpm may need to be calibrated to our new target
     armToPosition = new MoveArmToPosition(arm, Arm.speakerArmAngle);
+    searchNote = new SearchNote(swerveSubsystem, photonCamera);
   }
 
   //Method to switch between the diffrent states of the auto
   public void MoveToState(States newState) {
     switch(newState){
       case Search:
-        //Create a search command and put it here
+        searchNote.schedule();
         break;
 
       case Drive:
